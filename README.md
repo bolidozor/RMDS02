fc547f450a4ea3f0ca76c7bbfe83fb8c1134fccb
Jakub Kakona <kaklik@mlab.cz>
2017-07-31 13:12:20 +0200

    remove old files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# RMDS
-Radio Meteor Detection Station
--- usr/upload_to_fs.cmd
+++
@@ -1,4 +0,0 @@
-rem "C:\Program Files\WinSCP\WinSCP.exe" meteor_fs /synchronize c:\meteors\extra /home/meteor/meteors/SVAK1_/capture /defaults
-date /t
-time /t
-"C:\Program Files\WinSCP\WinSCP.exe" winscp.exe /console /script=c:\meteors\upload_to_fs.txt
